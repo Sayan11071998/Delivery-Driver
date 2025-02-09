3f794f47accd0cb6789c788cccedfaa4e1e9f937
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-10 03:21:56 +0530

    Follow Camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] public GameObject thingToFollow;
+
+    private Vector3 offset = new Vector3(0, 0, -10);
+
+    private void LateUpdate()
+    {
+        transform.position = thingToFollow.transform.position + offset;
+    }
+}
